873bac7fb40d77c2a92dd01747a789154cd050d9
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-07 12:23:30 -0300

    feat: adiciona view para lançar uma sonda

---
+++ mars_probe_api/probes/views.py
@@ -0,0 +1,39 @@
+from fastapi import Depends, HTTPException, status
+
+from mars_probe_api.probes.payloads import CreateProbePayload
+from mars_probe_api.probes.responses import CreateProbeResponse
+from mars_probe_api.probes.services import ProbeService
+from mars_probe_api.store.mysqlstore import SQLAlchemySession, get_db
+
+
+class CreateProbeView:
+    @staticmethod
+    def post(
+        probe_setup: CreateProbePayload,
+        session: SQLAlchemySession = Depends(get_db),
+    ) -> CreateProbeResponse:
+        """
+        View responsável pelo lançamento de uma sonda
+        Cria um sonda com sua posição atual, direção
+        e define as dimensões do planalto/malha
+        """
+
+        try:
+            probe = ProbeService.create_probe(
+                terrain_length=probe_setup.x,
+                terrain_width=probe_setup.y,
+                direction=probe_setup.direction,
+                session=session,
+            )
+
+            return CreateProbeResponse(
+                id=probe.id, x=probe.x, y=probe.y, direction=probe.direction
+            )
+        except ValueError as e:
+            raise HTTPException(status_code=status.HTTP_422_BAD_REQUEST, detail=str(e))
+
+        except Exception:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Internal server error",
+            )
