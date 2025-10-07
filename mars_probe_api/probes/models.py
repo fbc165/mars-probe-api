e04c26592e1b8283d3b81a6f9f97108d6c3bf448
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-07 11:30:52 -0300

    feat: adiciona model de uma sonda

---
+++ mars_probe_api/probes/models.py
@@ -0,0 +1,17 @@
+import uuid
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Probe(Base):
+    __tablename__ = "probes"
+
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    x = Column(Integer, nullable=False, default=0)
+    y = Column(Integer, nullable=False, default=0)
+    direction = Column(String(10), nullable=False)
+    terrain_length = Column(Integer, nullable=False)
+    terrain_width = Column(Integer, nullable=False)
