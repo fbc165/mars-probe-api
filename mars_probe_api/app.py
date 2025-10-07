be38ca7865745a5dac16950ddf5a1b614b8a3b88
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-07 12:37:34 -0300

    feat: adiciona router da sonda

--- mars_probe_api/app.py
+++ mars_probe_api/app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from mars_probe_api.probes.urls import router as probes_router
+
 app = FastAPI(
     title="Mars Probe API",
     description="API de controle de sondas em marte.",
@@ -11,3 +13,5 @@ app = FastAPI(
         },
     ],
 )
+
+app.include_router(probes_router, prefix="/probes", tags=["Probes"])
