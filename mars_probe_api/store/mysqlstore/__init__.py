c6dff03f6d0f3198918a1c350f3cf3f8d53125df
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-07 11:28:37 -0300

    feat: adiciona configuração do banco de dados

---
+++ mars_probe_api/store/mysqlstore/__init__.py
@@ -0,0 +1,19 @@
+from mars_probe_api.settings import SQLALCHEMY_DATABASE_URI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session as SQLAlchemySession
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URI,
+    pool_pre_ping=True,
+    pool_use_lifo=True,
+    pool_size=30,
+    max_overflow=15,
+    pool_recycle=1800,
+    echo=False,
+    connect_args={
+        "charset": "utf8mb4",
+        "autocommit": False,
+    },
+)
+Session = sessionmaker(bind=engine)
